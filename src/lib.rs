f6ec804042d6da21aacbdecbd6a468ee829d6cd3
Sven Nilsen <bvssvni@gmail.com>
2015-02-15 20:33:08 +0100

    Reexported "add" from vecmath

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
 #![deny(missing_docs)]
 
 //! A simple and type agnostic quaternion math designed for reexporting
+
+extern crate vecmath;
+
+/// Quaternion type alias.
+pub type Quaternion<T> = [T; 4];
+
+pub use vecmath::vec4_add as add;
+
