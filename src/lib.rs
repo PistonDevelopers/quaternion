bdf947aced93dd1b6c9101c79378bd328b07c6d9
Sven Nilsen <bvssvni@gmail.com>
2015-02-15 22:00:07 +0100

    Using scalar/vector notation
    Closes https://github.com/PistonDevelopers/quaternion/issues/7

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,17 @@ extern crate vecmath;
 use std::num::Float;
 
 /// Quaternion type alias.
-pub type Quaternion<T> = [T; 4];
+pub type Quaternion<T> = (T, [T; 3]);
 
-pub use vecmath::vec4_add as add;
+/// Adds two quaternions.
+#[inline(always)]
+pub fn add<T: Float>(
+    a: Quaternion<T>,
+    b: Quaternion<T>
+) -> Quaternion<T> {
+    use vecmath::vec3_add as add;
+    (a.0 + b.0, add(a.1, b.1))
+}
 
 /// Multiplies two quaternions.
 #[inline(always)]
@@ -22,21 +30,20 @@ pub fn mul<T: Float + Copy>(
     use vecmath::vec3_dot as dot;
     use vecmath::vec3_scale as scale;
 
-    let r1 = a[0];
-    let r2 = b[0];
-    let v1 = [a[1], a[2], a[3]];
-    let v2 = [b[1], b[2], b[3]];
-    let scalar = r1 * r2 - dot(v1, v2);
-    let vector = add(
-        add(scale(v2, r1), scale(v1, r2)),
-        cross(v1, v2)
-    );
-    [scalar, vector[0], vector[1], vector[2]]
+    (
+        a.0 * b.0 - dot(a.1, b.1),
+        add(
+            add(scale(b.1, a.0), scale(a.1, b.0)),
+            cross(a.1, b.1)
+        )
+    )
 }
 
 /// Takes the quaternion conjugate.
 #[inline(always)]
 pub fn conj<T: Float>(a: Quaternion<T>) -> Quaternion<T> {
-    [a[0], -a[1], -a[2], -a[3]]
+    use vecmath::vec3_neg as neg;
+
+    (a.0, neg(a.1))
 }
 
